1dcf04d6ba72611d30768ff2f7e26940b1325de8
Raphaël Jakse <raphael.jakse@algoo.fr>
2020-10-06 21:49:34 +0200

    WIP

--- frontend/src/container/NotificationWall.jsx
+++ frontend/src/container/NotificationWall.jsx
@@ -38,11 +38,31 @@ import {
 
 import { escape as escapeHtml } from 'lodash'
 
+const getNotificationLabelAndId = (notification) => {
+  if (!notification.content) {
+    return ['', 0]
+  }
+
+  const [entityType, /* eventType */ , contentType] = notification.type.split('.')
+  return ((contentType === TLM_SUB.COMMENT) || (entityType === TLM_ENTITY.MENTION && notification.content.type === CONTENT_TYPE.COMMENT))
+    ? [notification.content.parentLabel, notification.content.parentId]
+    : [notification.content.label, notification.content.id]
+}
+
 export class NotificationWall extends React.Component {
-  handleClickNotification = async (e, notificationId, notificationDetails) => {
+  constructor () {
+    super()
+    this.state = {
+      unfoldedNotificationGroup: {}
+    }
+  }
+
+  handleClickNotification = async (e, notificationId, notificationDetails, dontFollowLink = false) => {
     const { props } = this
 
-    if (!notificationDetails.url) {
+    if (dontFollowLink) {
+      e.preventDefault()
+    } else if (!notificationDetails.url) {
       if (notificationDetails.emptyUrlMsg) {
         props.dispatch(newFlashMessage(notificationDetails.emptyUrlMsg, notificationDetails.msgType || 'warning'))
       }
@@ -84,15 +104,7 @@ export class NotificationWall extends React.Component {
     const escapedAuthor = escapeHtml(notification.author)
     const escapedUser = notification.user ? escapeHtml(notification.user.publicName) : ''
 
-    const escapedContentLabel = (
-      notification.content
-        ? escapeHtml(
-          ((contentType === TLM_SUB.COMMENT) || (entityType === TLM_ENTITY.MENTION && notification.content.type === CONTENT_TYPE.COMMENT))
-            ? notification.content.parentLabel
-            : notification.content.label
-        )
-        : ''
-    )
+    const escapedContentLabel = escapeHtml(getNotificationLabelAndId(notification)[0])
 
     const escapedWorkspaceLabel = notification.workspace ? escapeHtml(notification.workspace.label) : ''
 
@@ -160,14 +172,16 @@ export class NotificationWall extends React.Component {
         return {
           icon: 'comment-o',
           text: props.t('{{author}} mentioned you in a comment in {{content}} in {{space}}', i18nOpts),
-          url: PAGE.WORKSPACE.CONTENT(notification.workspace.id, notification.content.parentContentType, notification.content.parentId)
+          url: PAGE.WORKSPACE.CONTENT(notification.workspace.id, notification.content.parentContentType, notification.content.parentId),
+          isMention: true
         }
       }
 
       return {
         icon: 'at',
         text: props.t('{{author}} mentioned you in {{content}} in {{space}}', i18nOpts),
-        url: contentUrl
+        url: contentUrl,
+        isMention: true
       }
     }
 
@@ -277,10 +291,75 @@ export class NotificationWall extends React.Component {
     }
   }
 
+  toggleGroup (spaceId) {
+    this.setState(prev => ({
+      unfoldedNotificationGroup: { ...prev.unfoldedNotificationGroup, [spaceId]: !this.state.unfoldedNotificationGroup[spaceId] }
+    }))
+  }
+
   render () {
-    const { props } = this
+    const { props, state } = this
 
     if (!props.notificationPage.list) return null
+
+    const notificationsGroupsByContentIdBySpaceId = {}
+    for (const notification of props.notificationPage.list) {
+      const spaceId = notification.workspace ? notification.workspace.id : '0'
+      const byContent = (
+        notificationsGroupsByContentIdBySpaceId[spaceId] || (
+          notificationsGroupsByContentIdBySpaceId[spaceId] = {
+            label: (
+              notification.workspace
+                ? notification.workspace.label
+                : '(instance)'
+            ),
+            id: spaceId,
+            byContent: {}
+          }
+        )
+      ).byContent
+      const [label, contentId] = (
+        notification.content
+          ? getNotificationLabelAndId(notification)
+          : ['(space)', 0]
+      )
+      ;(
+        byContent[contentId] || (
+          byContent[contentId] = {
+            label: label,
+            id: contentId,
+            list: []
+          }
+        )
+      ).list.push(notification)
+    }
+
+    const notificationsGroupListByContentListBySpace = []
+    for (const notificationsGroupsByContentId of Object.values(notificationsGroupsByContentIdBySpaceId)) {
+      const notificationsGroupsByContent = []
+
+      for (const notificationsGroup of Object.values(notificationsGroupsByContentId.byContent)) {
+        notificationsGroupsByContent.push({
+          list: notificationsGroup.list.sort(
+            (a, b) => Number(b.id) - Number(a.id)
+          ),
+          id: notificationsGroup.id,
+          label: notificationsGroup.label
+        })
+      }
+      notificationsGroupsByContent.sort((groupA, groupB) => groupB.list[0].id - groupA.list[0].id)
+      notificationsGroupListByContentListBySpace.push({
+        id: notificationsGroupsByContentId.id,
+        label: notificationsGroupsByContentId.label,
+        list: notificationsGroupsByContent
+      })
+    }
+
+    notificationsGroupListByContentListBySpace.sort(
+      (groupOfGroupsA, groupOfGroupsB) => (
+        groupOfGroupsB.list[0].list[0].id - groupOfGroupsA.list[0].list[0].id
+      )
+    )
 
     return (
       <div className={classnames('notification', { notification__wallClose: !props.isNotificationWallOpen })}>
@@ -300,61 +379,92 @@ export class NotificationWall extends React.Component {
           />
         </PopinFixedHeader>
 
-        <div className='notification__list'>
-          {props.notificationPage.list.length !== 0 && props.notificationPage.list.map((notification, i) => {
-            const notificationDetails = this.getNotificationDetails(notification)
-            const icons = notificationDetails.icon.split('+')
-            const icon = (
-              icons.length === 1
-                ? <i className={`fa fa-fw fa-${icons[0]}`} />
-                : <ComposedIcon mainIcon={icons[0]} smallIcon={icons[1]} />
-            )
+        <div className='notification__groups'>
+          {notificationsGroupListByContentListBySpace.map(({ label: spaceName, id: workspaceId, list: contentList }) => {
+            let spaceUnreadCount = 0
+            for (const content of contentList) {
+              spaceUnreadCount += content.list.filter(notification => !notification.read).length
+            }
 
             return (
-              <ListItemWrapper
-                isLast={i === props.notificationPage.list.length - 1}
-                read={false}
-                id={`${ANCHOR_NAMESPACE.notificationItem}:${notification.id}`}
-                key={notification.id}
-              >
-                <Link
-                  to={notificationDetails.url || '#'}
-                  onClick={(e) => this.handleClickNotification(e, notification.id, notificationDetails)}
-                  className={
-                    classnames('notification__list__item', { itemRead: notification.read })
-                  }
-                  key={notification.id}
-                >
-                  {icon}
-                  <div className='notification__list__item__text'>
-                    <Avatar publicName={notification.author} width={23} style={{ marginRight: '5px' }} />
-                    <span
-                      dangerouslySetInnerHTML={{
-                        __html: (
-                          notificationDetails.text + ' ' +
-                          `<span title='${escapeHtml(formatAbsoluteDate(notification.created, props.user.lang))}'>` +
-                            escapeHtml(displayDistanceDate(notification.created, props.user.lang)) +
-                          '</span>'
-                        )
-                      }}
-                    />
-                  </div>
-                  {!notification.read && <i className='notification__list__item__circle fa fa-circle' />}
+              <div className={'notification__group notification__group__' + (state.unfoldedNotificationGroup[workspaceId] ? 'un' : '') + 'folded'} key={spaceName}>
+                <Link onClick={() => this.toggleGroup(workspaceId)}>
+                  {spaceName}
+                  {spaceUnreadCount ? <b>{' (' + spaceUnreadCount + ')'}</b> : ''}
                 </Link>
-              </ListItemWrapper>
+                <div className='notification__groups'>
+                  {contentList.map(({ label: contentName, id: contentId, list: notificationList }) => {
+                    const contentUnreadCount = notificationList.filter(notification => !notification.read).length
+                    const groupId = workspaceId + '.' + contentId
+                    return (
+                      <div className={'notification__group notification__group__' + (state.unfoldedNotificationGroup[groupId] ? 'un' : '') + 'folded'} key={contentId}>
+                        <Link onClick={() => this.toggleGroup(groupId)}>
+                          {contentName}
+                          {contentUnreadCount ? <b>{' (' + contentUnreadCount + ')'}</b> : ''}
+                        </Link>
+                        <div className='notification__list'>
+                          {notificationList.map((notification, i) => {
+                            const notificationDetails = this.getNotificationDetails(notification)
+                            const icons = notificationDetails.icon.split('+')
+                            const icon = (
+                              icons.length === 1
+                                ? <i className={`fa fa-fw fa-${icons[0]}`} />
+                                : <ComposedIcon mainIcon={icons[0]} smallIcon={icons[1]} />
+                            )
+
+                            return (
+                              <ListItemWrapper
+                                isLast={i === props.notificationPage.list.length - 1}
+                                read={false}
+                                id={`${ANCHOR_NAMESPACE.notificationItem}:${notification.id}`}
+                                key={notification.id}
+                              >
+                                <Link
+                                  to={notificationDetails.url || '#'}
+                                  onClick={(e) => this.handleClickNotification(e, notification.id, notificationDetails)}
+                                  className={
+                                    classnames('notification__list__item', { itemRead: notification.read, isMention: notificationDetails.isMention })
+                                  }
+                                  key={notification.id}
+                                >
+                                  {icon}
+                                  <div className='notification__list__item__text'>
+                                    <Avatar publicName={notification.author} width={23} style={{ marginRight: '5px' }} />
+                                    <span
+                                      dangerouslySetInnerHTML={{
+                                        __html: (
+                                          notificationDetails.text + ' ' +
+                                          `<span title='${escapeHtml(formatAbsoluteDate(notification.created, props.user.lang))}'>` +
+                                            escapeHtml(displayDistanceDate(notification.created, props.user.lang)) +
+                                          '</span>'
+                                        )
+                                      }}
+                                    />
+                                  </div>
+                                  {!notification.read && <Link onClick={(e) => this.handleClickNotification(e, notification.id, notificationDetails, true)} className='notification__list__item__circle fa fa-circle' />}
+                                </Link>
+                              </ListItemWrapper>
+                            )
+                          })}
+                        </div>
+                      </div>
+                    )
+                  })}
+                </div>
+              </div>
             )
           })}
-
-          {props.notificationPage.hasNextPage &&
-            <div className='notification__footer'>
-              <GenericButton
-                customClass='btn outlineTextBtn primaryColorBorder primaryColorBgHover primaryColorBorderDarkenHover'
-                onClick={this.handleClickSeeMore}
-                label={props.t('See more')}
-                faIcon='chevron-down'
-              />
-            </div>}
         </div>
+        {(props.notificationPage.hasNextPage &&
+          <div className='notification__footer'>
+            <GenericButton
+              customClass='btn outlineTextBtn primaryColorBorder primaryColorBgHover primaryColorBorderDarkenHover'
+              onClick={this.handleClickSeeMore}
+              label={props.t('See more')}
+              faIcon='chevron-down'
+            />
+          </div>
+        )}
       </div>
     )
   }
