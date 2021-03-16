67e126e6e843cb3c3d1d9448d1e79d3c8d2e1a6b
Raphaël Jakse <raphael.jakse@algoo.fr>
2021-03-16 09:26:53 +0100

    feat(back, front): PoC kanban board

--- frontend/test/setup.js
+++ frontend/test/setup.js
@@ -40,6 +40,7 @@ if (!global.window && !global.document) {
   global.DOMParser = window.DOMParser
   global.Element = window.Element
   global.Audio = window.Audio
+  global.FormData = window.FormData
 }
 
 Enzyme.configure({ adapter: new Adapter() })
